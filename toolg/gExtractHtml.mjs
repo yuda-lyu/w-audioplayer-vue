ad6b47411b3728e1abffcab7be5733dbd3d55e1b
yuda-lyu <firsemisphere@gmail.com>
2020-06-01 15:18:03 +0800

    modify: update pks

--- toolg/gExtractHtml.mjs
+++ toolg/gExtractHtml.mjs
@@ -17,6 +17,7 @@ let h = `
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>example for {{casename}}</title>
 
     <!-- vue -->
