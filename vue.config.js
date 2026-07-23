2452b59b52afd7d444763c96a19a16a94d3bf14d
yuda-lyu <firsemisphere@gmail.com>
2026-07-23 10:56:45 +0800

    modify: update pks

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,6 @@ module.exports = {
     lintOnSave: false, //禁止eslint-loader於編譯時檢查語法
     devServer: {
         proxy: {
-            '/api': {
-                target: 'http://localhost:9000',
-                pathRewrite: {
-                    '^/api': '/api' 
-                },
-            },
-            '/swdoc': {
-                target: 'http://localhost:9000',
-                pathRewrite: {
-                    '^/swdoc': '/swdoc' 
-                },
-            },
         }
     },
     transpileDependencies: ['vuetify'],
