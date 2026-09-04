cdc961cd8de12c3fb16ebb91df63968f2e1ef154
yuda-lyu <firsemisphere@gmail.com>
2026-09-04 23:49:15 +0800

    modify: update pks

--- dist/w-audioplayer-vue.umd.js
+++ dist/w-audioplayer-vue.umd.js
@@ -1,5 +1,5 @@
 /*!
- * w-audioplayer-vue v2.0.32
+ * w-audioplayer-vue v2.0.33
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- docs/examples/app.umd.js
+++ docs/examples/app.umd.js
@@ -1,5 +1,5 @@
 /*!
- * app v2.0.32
+ * app v2.0.33
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
